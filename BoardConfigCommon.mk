b39a8aece47a446913a9594cc680e09de2f22553
Kumar Anand <kumaranand@google.com>
2018-01-22 15:02:54 -0800

    sm8350-common: Enable automatic channel selection
     * Automatic Channel Selection algorithms and implementations
       are used to enable interfaces to automatically figure
       out which channel configuration to use for initiating
       communication, for any mode of operation which initiates
       radiation (AP, Mesh, IBSS, P2P).
    
       https://wireless.wiki.kernel.org/en/users/documentation/acs
    
    Change-Id: I177a2c43922ff0579cf8cd4a69ac2c94c56aa0c2
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -191,6 +191,7 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 CONFIG_IEEE80211AC := true
 WIFI_FEATURE_HOSTAPD_11AX := true
+CONFIG_ACS := true
 
 # Inherit proprietary blobs
 include vendor/xiaomi/sm8350-common/BoardConfigVendor.mk
