90ae3da11d4046bbc003fb1c0856b26e0529cc07
Wei Wang <wvw@google.com>
2022-04-25 20:56:35 -0400

    sm8350-common: Drop QTI perfd stack
    Bug: 69270928
    Test: Build and boot device and test camera
    Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4

--- common.mk
+++ common.mk
@@ -343,7 +343,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
