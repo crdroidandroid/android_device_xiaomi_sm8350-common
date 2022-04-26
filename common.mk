4f28074ba95cedf6694b73200af49452867c4622
Lucchetto <lucchetto.tie@live.com>
2022-04-25 20:56:35 -0400

    sm8350-common: create dummy libqti-perfd-client
    * This way, proprietary perfd blobs can be removed without breaking
      fingerprint
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- common.mk
+++ common.mk
@@ -343,7 +343,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
