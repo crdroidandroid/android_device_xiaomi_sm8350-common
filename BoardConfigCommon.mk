7e85f6ed0533a3111f48295d20396481db165363
Kuba Wojciechowski <nullbytepl@gmail.com>
2022-06-10 20:36:28 +0530

    sm8350-common: Disable kpti
    * As per qualcomm - "SM8150/SM8250/SM8350/SM7250/SM7150/SM6150 - KPTI Not
    required".
    * It can also help increase performance by a lot in some scenarios.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,6 +101,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=0
 BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE += kpti=off
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
