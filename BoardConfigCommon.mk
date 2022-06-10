97776cff903272ae3765caa5dd86e21af90ed050
FlowerSea0208 <zzp060208@gmail.com>
2022-06-10 19:44:17 +0800

    sm8350-common: First Snow

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,7 +77,7 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/hidl/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/evolution/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/hidl/manifest_lahaina.xml \
