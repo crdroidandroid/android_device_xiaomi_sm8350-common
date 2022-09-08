6c32951da4e0cf1d7926d2db2cc4c640bf984c15
Joey Huab <joey@evolution-x.org>
2022-09-08 23:02:46 +0000

    sm8350-common: switch to AOSP BT HAL
    Signed-off-by: chemusqui1 <61394758+chemusqui1@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -93,15 +93,12 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
-    libbthost_if
+    audio.bluetooth.default
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     com.dsi.ant@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
