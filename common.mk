52a389e7bc457a05be8b2e6a36c4685be499a5cd
Giovanni Ricca <giovanniricca@proton.me>
2022-11-04 12:22:21 +0100

    sm8350-common: Enable Android Go configurations
    * They don't have a splitted authorship so take it :D
    https://github.com/LineageOS/android_build/commit/0a0e5ea5338db8103eec54ae7bdf9b58f8f7a93f
    
    Change-Id: I6f4207d0f8f105ec814777479457ade6d7d6a0f5
    [Tashar02: Do not set system server compiler filter to speed-profile
    	   since that is default, starting Android 14]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- common.mk
+++ common.mk
@@ -24,6 +24,14 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Always preopt extracted APKs to prevent extracting out of the APK for gms modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# Use a profile based boot image for this device. Note that this is currently a generic
+# profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Reduce system server verbosity
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
 
