9796ce0ec6ac644b0a6af87c77aa41976ae66d53
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    sm8350-common: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- common.mk
+++ common.mk
@@ -331,6 +331,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 ifneq ("$(wildcard hardware/lineage/livedisplay)", "")
 # LiveDisplay
 PRODUCT_PACKAGES += \
