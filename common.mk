fdaea4a8b58ccc83a50219c9d6100a47b2e763e0
Georg Veichtlbauer <georg@vware.at>
2023-03-09 15:34:20 +0100

    sm8350-common: Remove unneeded NFC package
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- common.mk
+++ common.mk
@@ -374,8 +374,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
     Tag
 
 PRODUCT_PACKAGES += \
