2ac614aff995efbe9578f51c1aa6e7730f71a30b
Martin Liu <liumartin@google.com>
2023-09-12 16:02:17 +0800

    sm8350-common: Tweak memory management
    - Set lmkd's swap_free_low_percentage to 100.
    - Decrease swappiness to 60.
    
    This tweak also aligns the policy with P23.
    
    Test: boots with correct settings
    Bug: 299975853
    Change-Id: I0ed7088ddf2ff2fa0feca2197e5cc03c7f5c6778
    Signed-off-by: Martin Liu <liumartin@google.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- rootdir/bin/init.kernel.post_boot-lahaina.sh
+++ rootdir/bin/init.kernel.post_boot-lahaina.sh
@@ -98,7 +98,7 @@ function configure_memory_parameters() {
 	#
 
 	configure_zram_parameters
-	echo 100 > /proc/sys/vm/swappiness
+	echo 60 > /proc/sys/vm/swappiness
 	echo 1 > /proc/sys/vm/watermark_scale_factor
 
 	# add memory limit to camera cgroup
--- rootdir/bin/init.kernel.post_boot-yupik.sh
+++ rootdir/bin/init.kernel.post_boot-yupik.sh
@@ -100,7 +100,7 @@ function configure_memory_parameters() {
 	ProductName=`getprop ro.product.name`
 
 	configure_zram_parameters
-	echo 100 > /proc/sys/vm/swappiness
+	echo 60 > /proc/sys/vm/swappiness
 
         # Disable wsf  beacause we are using efk.
         # wsf Range : 1..1000. So set to bare minimum value 1.
