471cdd4eeddb5620b5c7a9ea568f53970993a16e
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    sm8350-common: Build android.frameworks.sensorservice@1.0.vendor
    Required by mm-pp-deps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- common.mk
+++ common.mk
@@ -444,6 +444,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.1-service.xiaomi_sm8350-multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
