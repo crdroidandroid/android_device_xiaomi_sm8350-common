6b01a09d1a28abde169d19c7c3accafc37473186
aleeeee1 <pace.alessandro05@protonmail.com>
2024-04-10 18:32:39 +0000

    sm8350-commons:add right namespaces to power-mode
    Fixes
    
    Output:
    ld.lld: error: undefined symbol: _ZN4aidl6google8hardware5power4impl5pixel21setDeviceSpecificModeENS_7android8hardware5power4ModeEb
    >>> referenced by Power.cpp:88 (hardware/xiaomi/aidl/power-libperfmgr/Power.cpp:88)
    >>>               /home/ubuntu/rom/blaze/out/target/product/laurel_sprout/obj/EXECUTABLES/android.hardware.power-service.xiaomi-libperfmgr_intermediates/Power.o:(_ZN4aidl6google8hardware5power4impl5pixel5Power7setModeENS_7android8hardware5power4ModeEb)
    
    ld.lld: error: undefined symbol: _ZN4aidl6google8hardware5power4impl5pixel29isDeviceSpecificModeSupportedENS_7android8hardware5power4ModeEPb
    >>> referenced by Power.cpp:139 (hardware/xiaomi/aidl/power-libperfmgr/Power.cpp:139)
    >>>               /home/ubuntu/rom/blaze/out/target/product/laurel_sprout/obj/EXECUTABLES/android.hardware.power-service.xiaomi-libperfmgr_intermediates/Power.o:(_ZN4aidl6google8hardware5power4impl5pixel5Power15isModeSupportedENS_7android8hardware5power4ModeEPb)
    clang++: error: linker command failed with exit code 1 (use -v to see invocation)
    
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -18,10 +18,11 @@
 #define TOUCH_IOC_SETMODE TOUCH_MAGIC + SET_CUR_VALUE
 
 namespace aidl {
-namespace android {
+namespace google {
 namespace hardware {
 namespace power {
 namespace impl {
+namespace pixel {
 
 using ::aidl::android::hardware::power::Mode;
 
@@ -49,8 +50,9 @@ bool setDeviceSpecificMode(Mode type, bool enabled) {
     }
 }
 
+}  // namespace pixel
 }  // namespace impl
 }  // namespace power
 }  // namespace hardware
-}  // namespace android
+}  // namespace google
 }  // namespace aidl
