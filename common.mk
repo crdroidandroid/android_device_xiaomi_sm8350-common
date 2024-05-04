56a54ba496061cd69c48de5a2de4d631df8729ec
RobertGarciaa <chae0218@naver.com>
2024-05-04 13:00:58 +0900

    sm8350-common: Include MIUI Camera(renoir)
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- common.mk
+++ common.mk
@@ -118,6 +118,8 @@ PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
 # Camera
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
