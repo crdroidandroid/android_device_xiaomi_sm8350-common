c16d873def5c2d210b7144379c005eab72d1be27
Bruno Martins <bgcngm@gmail.com>
2024-05-18 13:21:42 +0100

    sm8350-common: Use common libqti-perfd-client and power-libperfmgr
    Change-Id: I22d6b35c72629553817005deaefcae60abc72cfd
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- common.mk
+++ common.mk
@@ -1,5 +1,6 @@
 #
 # SPDX-FileCopyrightText: The LineageOS Project
+#
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -410,7 +411,11 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/xiaomi \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \
