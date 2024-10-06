677f6eeab17473699ae0e447306e2dfc12d6d083
Arian <arian.kulmer@web.de>
2024-10-06 12:25:37 +0200

    sm8350-common: Import qcril_database and generate the db at build time
    Imported from haydn V816.0.6.0.UKKMIXM
    
    Change-Id: I7798bd3c33d3b6f801e8f48a06fe994947715db9
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- common.mk
+++ common.mk
@@ -378,7 +378,8 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     qti_telephony_utils.xml \
     telephony-ext \
-    xiaomi-telephony-stub
+    xiaomi-telephony-stub \
+    qcrilNrDb_vendor
 
 PRODUCT_BOOT_JARS += \
     telephony-ext \
