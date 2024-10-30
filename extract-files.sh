6436129825cc3b604b02362f1078a5a4850404e2
RobertGarciaa <chae0218@naver.com>
2024-10-31 01:09:30 +0900

    sm8350-common: Shim libwvdrmengine with libcrypto_shim
    out/soong/.intermediates/vendor/xiaomi/sm8350-common/libwvdrmengine/android_vendor_arm64_armv8-2a_shared/e560d7b19ebf7276b3e850d3d346dec8/libwvdrmengine.so: error: Unresolved symbol: CBS_init
    out/soong/.intermediates/vendor/xiaomi/sm8350-common/libwvdrmengine/android_vendor_arm64_armv8-2a_shared/e560d7b19ebf7276b3e850d3d346dec8/libwvdrmengine.so: note:
    out/soong/.intermediates/vendor/xiaomi/sm8350-common/libwvdrmengine/android_vendor_arm64_armv8-2a_shared/e560d7b19ebf7276b3e850d3d346dec8/libwvdrmengine.so: note: Some dependencies might be changed, thus the symbol(s) above cannot be resolved.
    out/soong/.intermediates/vendor/xiaomi/sm8350-common/libwvdrmengine/android_vendor_arm64_armv8-2a_shared/e560d7b19ebf7276b3e850d3d346dec8/libwvdrmengine.so: note: Please re-build the prebuilt file: "out/soong/.intermediates/vendor/xiaomi/sm8350-common/libwvdrmengine/android_vendor_arm64_armv8-2a_shared/e560d7b19ebf7276b3e850d3d346dec8/libwvdrmengine.so".
    out/soong/.intermediates/vendor/xiaomi/sm8350-common/libwvdrmengine/android_vendor_arm64_armv8-2a_shared/e560d7b19ebf7276b3e850d3d346dec8/libwvdrmengine.so: note:
    out/soong/.intermediates/vendor/xiaomi/sm8350-common/libwvdrmengine/android_vendor_arm64_armv8-2a_shared/e560d7b19ebf7276b3e850d3d346dec8/libwvdrmengine.so: note: If this is a new prebuilt file and it is designed to have unresolved symbols, add one of the following properties:
    out/soong/.intermediates/vendor/xiaomi/sm8350-common/libwvdrmengine/android_vendor_arm64_armv8-2a_shared/e560d7b19ebf7276b3e850d3d346dec8/libwvdrmengine.so: note:   Android.bp: allow_undefined_symbols: true,
    out/soong/.intermediates/vendor/xiaomi/sm8350-common/libwvdrmengine/android_vendor_arm64_armv8-2a_shared/e560d7b19ebf7276b3e850d3d346dec8/libwvdrmengine.so: note:   Android.mk: LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
    
    like this commit
    https://github.com/LineageOS/android_device_xiaomi_sm8350-common/commit/8ccab8a111e63ee30d80e3fb0ef864861d3c7751
    
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- extract-files.sh
+++ extract-files.sh
@@ -96,6 +96,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;
