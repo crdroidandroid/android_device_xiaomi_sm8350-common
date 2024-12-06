6341841433ba9984f5035a3a2f2f3c380cee2877
COSMIC <rayavanindra@gmail.com>
2024-12-06 18:08:59 +0530

    sm8350-common: Patch libwfdservice to load android.media.audio.common.types-V4-cpp.so
    Fixes:
    error: module "libwfdservice" variant "android_arm64_armv8-2a_cortex-a76_static":
    depends on multiple versions of the same aidl_interface:
    android.media.audio.common.types-V3-cpp-source,
    android.media.audio.common.types-V4-cpp-source
    
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- extract-files.sh
+++ extract-files.sh
@@ -70,7 +70,7 @@ function blob_fixup() {
             ;;
         system_ext/lib64/libwfdservice.so|system_ext/lib/libwfdservice.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V4-cpp.so" "${2}"
             ;;
         system_ext/lib64/libwfdnative.so)
             [ "$2" = "" ] && return 0
