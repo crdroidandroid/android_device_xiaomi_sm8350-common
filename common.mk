d7c49ff15325c94b10079564ee54f92148d1c870
RobertGarciaa <chae0218@naver.com>
2024-12-15 23:42:44 +0900

    sm8350-common: Build more soundtrigger packages
    W libc    : Unable to set property "ctl.interface_start" to "android.hardware.soundtrigger@2.0::ISoundTriggerHw/default": PROP_ERROR_HANDLE_CONTROL_MESSAGE (0x20)
    
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- common.mk
+++ common.mk
@@ -43,7 +43,10 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt
