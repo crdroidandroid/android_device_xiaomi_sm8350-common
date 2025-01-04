335790c319be197eb88fc7654dc97beb69a5ac0f
kamikaonashi <thorschi93@gmx.at>
2025-01-04 22:14:41 +0100

    sm8350-common: parts: Import DC Dimming and HBM Settings
    * Implement DC Dimming preference
    * Disable DC Dimming if it's not supported
    * Implement HBM switch
    * Adapt HBM and DC dimming settings to S style
    * Define DC dimming and HBM nodes / keys in one place
    * Declare exported flags in manifest
    * Fix compilation for QPR1
    * Fix compilation for Android 15
    * Adapt to sm8350-common devices
    
    Co-Authored-By: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-Authored-By: Adhitya Mohan <me@adhityamohan.in>
    Co-Authored-By: TheScarastic <warabhishek@gmail.com>
    Co-Authored-By: alibei <sebastiankotzias83@gmail.com>
    Co-Authored-By: kamikaonashi <thorschi93@gmx.at>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -21,21 +21,33 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
+import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_exposure";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+    SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DozeUtils.onBootCompleted(context);
         ThermalUtils.startService(context);
-        RefreshUtils.startService(context);        
+        RefreshUtils.startService(context);    
+        
+        // DC Dimming
+        FileUtils.enableService(context);
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
     }
 }
