46a324dd7e571d73cc1f24f7e04edafb63ac5d21
Yumi Yukimura <me.cafebabe@gmail.com>
2025-01-09 18:14:39 +0100

    sm8350-common: Switch to QTI Memtrack AIDL HAL

--- common.mk
+++ common.mk
@@ -145,17 +145,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.composer-service.rc \
-    vendor.qti.hardware.display.composer-service.xml
+    vendor.qti.hardware.display.composer-service.xml \
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@3.0 \
-    libqdMetaData.system \
-    memtrack.default
+    libqdMetaData.system
 
 PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
