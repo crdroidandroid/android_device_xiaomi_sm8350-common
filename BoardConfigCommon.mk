25026523dacbe64ae9d774661360bda09cd16fc6
New Author Name <120115258+swiitchOFF@users.noreply.github.com>
2025-03-06 01:21:20 +0530

    sm8350-common: Include Sony Dolby Atmos
    * Using dolby from: https://github.com/swiitch-OFF-Lab/hardware_dolby
    [1]: sm8350-common: Add dolby volume listener effect
    [2]: sm8350-common: audio: Bring all stream effects
    
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,7 +65,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/framework_compatibility_matrix.xml
 
-DEVICE_MANIFEST_FILE := \
+DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/hidl/manifest_lahaina.xml \
     $(COMMON_PATH)/hidl/manifest_xiaomi.xml
 
--- common.mk
+++ common.mk
@@ -153,6 +153,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
