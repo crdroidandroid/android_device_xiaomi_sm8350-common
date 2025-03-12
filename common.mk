0dc55e0c36837c229223e9c9d8a7cfba51e80dc9
INCENDIOCODM <vinayakgamemaster42@gmail.com>
2025-03-12 10:12:14 +0530

    sm8350-common: remove unsupported libs

--- common.mk
+++ common.mk
@@ -99,9 +99,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     com.dsi.ant@1.0.vendor \
-    libbthost_if \
-    libbthost_if.vendor \
-    libldacBT_bco.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
