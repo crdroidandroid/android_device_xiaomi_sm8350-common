0fa8d1e187a8b486b5fdcf7f940998fa07dd5e89
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-03-25 02:30:05 +0600

    sm8350-common: Use new location of dex2oat boot image profile
    config/boot-image-profile.txt has been dropped in favor of
    boot/boot-image-profile.txt for all since
    https://github.com/PixelOS-AOSP/frameworks_base/commit/6431bd08c47b93bdaee339d270287cbe36982e8d
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- common.mk
+++ common.mk
@@ -30,7 +30,7 @@ PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 # Use a profile based boot image for this device. Note that this is currently a generic
 # profile and not Android Go optimized.
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Reduce system server verbosity
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
