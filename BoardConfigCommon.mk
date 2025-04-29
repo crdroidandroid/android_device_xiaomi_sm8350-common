8631a74f56681347e7b6b8fd6a335354e781c5f2
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:11:42 +0100

    sm8350-common: Move libperfmgr mode extension lib to select()
    Change-Id: I39ac8867d98808832738f58eb6f50d456d6d34c4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,9 +135,6 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 # Platform
 TARGET_BOARD_PLATFORM := lahaina
 
-# Power
-TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(COMMON_PATH):libperfmgr-ext-xiaomi
-
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
