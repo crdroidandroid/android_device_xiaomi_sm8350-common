a5d4bbbe2dd891807eee13db4ef7271997c181e4
LuK1337 <priv.luk@gmail.com>
2025-07-20 11:06:53 +0200

    sm8350-common: CONFIG_IEEE80211AX -> WIFI_FEATURE_HOSTAPD_11AX
    Test: Start hotspot, notice that it uses 11ax instead of 11ac.
    Change-Id: Ie3c5fe9cfe39c28ba1df2be4e676954aca78f67a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,7 +206,7 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 CONFIG_ACS := true
 CONFIG_IEEE80211AC := true
-CONFIG_IEEE80211AX := true
+WIFI_FEATURE_HOSTAPD_11AX := true
 
 # Inherit proprietary blobs
 include vendor/xiaomi/sm8350-common/BoardConfigVendor.mk
