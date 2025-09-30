b7ab500ac9ae593ad60a4b6910a39997c79e0028
Giovanni Ricca <giovanniricca@proton.me>
2025-09-30 11:00:43 +0200

    sm8350-common: Build more audio extension from source
    Change-Id: Icca1995c5cfa796ab6367e73b1dc217373d1a33d

--- common.mk
+++ common.mk
@@ -48,9 +48,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     liba2dpoffload \
     libbatterylistener \
+    libexthwplugin \
     libhdmiedid \
     libhfp \
-    libsndmonitor
+    libsndmonitor \
+    libspkrprot \
+    libssrec
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt
