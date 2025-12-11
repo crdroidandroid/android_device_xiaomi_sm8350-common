4d3591c709ffbec0f2522213845618bebd8cd81d
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    sm8350-common: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -58,6 +58,11 @@ blob_fixups: blob_fixups_user_type = {
         .remove_needed('android.hidl.base@1.0.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
+    (
+        'vendor/lib64/libdpps.so',
+        'vendor/lib64/libsnapdragoncolor-manager.so',
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
