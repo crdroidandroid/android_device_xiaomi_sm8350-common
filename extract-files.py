2ea696922771710b4b34868f08e41a9977f23856
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-04-06 07:53:39 +0530

    sm8350-common: Update standalone Dolby Vision blobs
    Change-Id: I0ace47adba0c7d733249ad48bc9ef2fd9f7733b8

--- extract-files.py
+++ extract-files.py
@@ -64,12 +64,10 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcrypto_shim.so'),
     'vendor/lib64/android.hardware.secure_element@1.0-impl.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
-        ('vendor/lib64/c2.dolby.avc.dec.so', 'vendor/lib64/c2.dolby.avc.sec.dec.so', 'vendor/lib64/c2.dolby.hevc.dec.so', 'vendor/lib64/c2.dolby.hevc.sec.dec.so'): blob_fixup()
+    ('vendor/lib64/c2.dolby.avc.dec.so', 'vendor/lib64/c2.dolby.avc.sec.dec.so', 'vendor/lib64/c2.dolby.hevc.dec.so', 'vendor/lib64/c2.dolby.hevc.sec.dec.so'): blob_fixup()
         .add_needed('libcodec2_shim.so')
         .add_needed('libstagefright_foundation-v33.so'),
-        ('vendor/bin/hw/dolbycodec2'): blob_fixup()
-        .add_needed('libshim_dolby.so'),
-        ('vendor/lib64/c2.dolby.client.so'): blob_fixup()
+    ('vendor/lib64/c2.dolby.client.so'): blob_fixup()
         .add_needed('libcodec2_shim.so')
         .add_needed('libcodec2_hidl_shim.so'),
     'vendor/etc/qcril_database/upgrade/config/6.0_config.sql': blob_fixup()
